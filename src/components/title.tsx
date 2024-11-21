c7e1f1ee03f7884c4d3962d4b28679c6474045f5
Devin Sweeting <devinsweeting@gmail.com>
2024-11-21 13:11:37 -0800

    Set up page layout, css styles, and create title

---
+++ src/components/title.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from "react";
+import localFont from "next/font/local";
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+const libreBarcode = localFont({
+  src: "../app/fonts/LibreBarcode128.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+// border-2 border-red-200
+export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+  return <div className={` w-full sm:w-2/3 float-left p-8`}>{children}</div>;
+};
+
+export default function Title() {
+  return (
+    <Wrapper>
+      <div className={`${libreBarcode.className} text-6xl bg-transparent`}>
+        Devin Sweeting
+      </div>
+    </Wrapper>
+  );
+}
